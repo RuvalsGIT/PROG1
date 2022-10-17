1c0b2cf1fe89cfefa1af16538954ff7e19c21520
vrube <Rube19940@hs-ansbach.de>
2022-10-17 15:51:23 +0200

    Version 1

---
+++ src/ScannerExample.java
@@ -0,0 +1,13 @@
+public class ScannerExample {
+    public static void main (String[] args) {
+        System.out.print("Gib bitte Deinen Namen ein: ");
+        String name = new java.util.Scanner(System.in).nextLine();
+
+        System.out.print("Hallo, " + name + "! Wie alt bist Du denn= ");
+        int age = new java.util.Scanner(System.in).nextInt();
+
+        System.out.println( "Schon " +  " Jahre alt?"
+                + "Du hast nur noch " + (67 - age)
+                + " Jahre bis zur Rente!");
+    }
+}
