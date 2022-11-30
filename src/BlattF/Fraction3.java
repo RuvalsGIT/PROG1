d3ff56b349b7b4269e3e9f450e64e8b7cbd3dba3
vrube <Rube19940@hs-ansbach.de>
2022-11-30 12:03:37 +0100

    Update 30_11_2022

--- src/BlattF/Fraction3.java
+++ src/BlattF/Fraction3.java
@@ -1,34 +1,53 @@
 package BlattF;
 
 public class Fraction3 {
-        int numerator;
-        int denominator;
+    int numerator;
+    int denominator;
+
+    int reducery;
+
+    void add(Fraction3 op) {
+        numerator = (numerator * op.denominator) +
+                (op.numerator * denominator);
+        denominator = denominator * op.denominator;
+    }
+
+    void sub(Fraction3 op) {
+        numerator = (numerator * op.denominator) -
+                (op.numerator * denominator);
+        denominator = denominator * op.denominator;
+    }
+
+    void mul(Fraction3 op) {
+        numerator = numerator * op.numerator;
+        denominator = denominator * op.denominator;
+    }
+
+    void div(Fraction3 op) {
+        numerator = numerator * op.denominator;
+        denominator = denominator * op.numerator;
+    }
+
+    double toDouble() {
+        return (double) numerator / (double) denominator;
+    }
 
 
-        void add(Fraction3 op) {
-            numerator = (numerator * op.denominator) +
-                    (op.numerator * denominator);
-            denominator = denominator * op.denominator;
-        }
-
-        void sub(Fraction3 op) {
-            numerator = (numerator * op.denominator) -
-                    (op.numerator * denominator);
-            denominator = denominator * op.denominator;
-        }
-
-        void mul(Fraction3 op) {
-            numerator = numerator * op.numerator;
-            denominator = denominator * op.denominator;
-        }
-
-        void div (Fraction3 op) {
-            numerator = numerator * op.denominator;
-            denominator = denominator * op.numerator;
-        }
-
-            double toDouble() {
-                return (double) numerator / (double) denominator;
+        void reduce(Fraction3 op) {
+            if (numerator == 0){
+                reducery = denominator;
             }
+            else {
+                while (denominator != 0){
+                    if(numerator > denominator) {
+                        numerator = numerator - denominator;
+                    }
+                    else{
+                        denominator = denominator - numerator;
+                        reducery = numerator;
+                    }
+                }
+            }
+        }
     }
 
--- src/BlattF/Fraction3Main.java
+++ src/BlattF/Fraction3Main.java
@@ -15,22 +15,41 @@ public class Fraction3Main {
         System.out.println(myFraction1.toDouble());
         System.out.println(myFraction2.toDouble());
 
-        myFraction1.add(myFraction2);
-        System.out.println( "ADD " + myFraction1.numerator + "/" + myFraction1.denominator);
-        System.out.println(myFraction1.toDouble());
+        System.out.println(" Wählen sie zwischen, ADD / SUB / MUL / DIV");
+        String operator = new java.util.Scanner(System.in).nextLine();
 
-        myFraction1.sub(myFraction2);
-        System.out.println("SUB " + myFraction1.numerator + "/" + myFraction1.denominator);
-        System.out.println(myFraction1.toDouble());
+        switch (operator) {
 
-        myFraction1.mul(myFraction2);
-        System.out.println("MUL " + myFraction1.numerator + "/" + myFraction1.denominator);
-        System.out.println(myFraction1.toDouble());
+            case "ADD":
+                myFraction1.add(myFraction2);
+                System.out.println("ADD " + myFraction1.numerator + "/" + myFraction1.denominator);
+                System.out.println(myFraction1.toDouble());
+                break;
 
-        myFraction1.div(myFraction2);
-        System.out.println("DIV " + myFraction1.numerator + "/" + myFraction1.denominator);
-        System.out.println(myFraction1.toDouble());
+            case "SUB":
+                myFraction1.sub(myFraction2);
+                System.out.println("SUB " + myFraction1.numerator + "/" + myFraction1.denominator);
+                System.out.println(myFraction1.toDouble());
+                break;
 
+            case "MUL":
+                myFraction1.mul(myFraction2);
+                System.out.println("MUL " + myFraction1.numerator + "/" + myFraction1.denominator);
+                myFraction1.reduce(myFraction1);
+                System.out.println(myFraction1.toDouble());
+                break;
+
+            case "DIV":
+                myFraction1.div(myFraction2);
+                System.out.println("DIV " + myFraction1.numerator + "/" + myFraction1.denominator);
+                System.out.println(myFraction1.toDouble());
+                break;
+
+        }
+        Fraction3 reducer = new Fraction3();
+        reducer.reduce(myFraction1);
+        System.out.println(reducer);
     }
+
     }
 
